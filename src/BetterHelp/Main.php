958ea9d0797d5922a70409a24dc6a937a7cfa648
ondraczsk <ondrakraftak@gmail.com>
2016-12-11 20:31:37 +0100

    Update Main.php

--- src/BetterHelp/Main.php
+++ src/BetterHelp/Main.php
@@ -1,1 +1,22 @@
+<?php
 
+namespace ondraczsk\BetterHelp;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandExecutor;
+use pocketmine\command\CommandSender;
+
+// My first non-copied plugin! I learning from Plugin school web :P
+
+class Main extends PluginBase{
+  public function onLoad(){
+    $this->getLogger()->info("Plugin Napoveda se nacita..");
+  }
+  public function onEnable(){
+    $this->getLogger()->info("Plugin Napoveda zapnut..");
+  }
+  public function onDisable(){
+    $this->getLogger()->info("Plugin Napoveda vypnut..");
+
+   
+}
