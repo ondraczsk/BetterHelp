9ae16f2d6ea7091b2da9cb51495985f130358221
ondraczsk <ondrakraftak@gmail.com>
2016-12-12 16:25:07 +0100

    Update BetterHelp.php

--- src/BetterHelp/BetterHelp.php
+++ src/BetterHelp/BetterHelp.php
@@ -5,10 +5,51 @@ namespace BetterHelp;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\Player;
+use pocketmine\utils\TextFormat;
 use pocketmine\Server;
 use pocketmine\event\Listener;
+use pocketmine\utils\Config;
 
 class BetterHelp extends PluginBase implements Listener{
+          
+          
+     /**
+     * Translate Minecraft colors
+     * 
+     * @param char $symbol Color symbol
+     * @param string $message The message to be translated
+     * 
+     * @return string The translated message
+     */
+    public function translateColors($symbol, $message){
+    
+    	$message = str_replace($symbol."0", TextFormat::BLACK, $message);
+    	$message = str_replace($symbol."1", TextFormat::DARK_BLUE, $message);
+    	$message = str_replace($symbol."2", TextFormat::DARK_GREEN, $message);
+    	$message = str_replace($symbol."3", TextFormat::DARK_AQUA, $message);
+    	$message = str_replace($symbol."4", TextFormat::DARK_RED, $message);
+    	$message = str_replace($symbol."5", TextFormat::DARK_PURPLE, $message);
+    	$message = str_replace($symbol."6", TextFormat::GOLD, $message);
+    	$message = str_replace($symbol."7", TextFormat::GRAY, $message);
+    	$message = str_replace($symbol."8", TextFormat::DARK_GRAY, $message);
+    	$message = str_replace($symbol."9", TextFormat::BLUE, $message);
+    	$message = str_replace($symbol."a", TextFormat::GREEN, $message);
+    	$message = str_replace($symbol."b", TextFormat::AQUA, $message);
+    	$message = str_replace($symbol."c", TextFormat::RED, $message);
+    	$message = str_replace($symbol."d", TextFormat::LIGHT_PURPLE, $message);
+    	$message = str_replace($symbol."e", TextFormat::YELLOW, $message);
+    	$message = str_replace($symbol."f", TextFormat::WHITE, $message);
+    
+    	$message = str_replace($symbol."k", TextFormat::OBFUSCATED, $message);
+    	$message = str_replace($symbol."l", TextFormat::BOLD, $message);
+    	$message = str_replace($symbol."m", TextFormat::STRIKETHROUGH, $message);
+    	$message = str_replace($symbol."n", TextFormat::UNDERLINE, $message);
+    	$message = str_replace($symbol."o", TextFormat::ITALIC, $message);
+    	$message = str_replace($symbol."r", TextFormat::RESET, $message);
+              
+              
+    	return $message;
+    }
 
           public function onLoad(){
                     $this->getLogger()->info("Plugin Loading");
@@ -16,10 +57,20 @@ class BetterHelp extends PluginBase implements Listener{
           public function onEnable(){
                     $this->getLogger()->info("Enabled Plugin");
                     $this->getServer()->getPluginManager()->registerEvents($this,$this);
+                    @mkdir($this->getDataFolder());
+                    $this->config = new Config ($this->getDataFolder() . "config.yml" , Config::YAML, array());
           }
           public function onJoin(PlayerJoinEvent $event){
-   $player = $event->getPlayer();
-   $name = $player->getName();
+                    $player = $event->getPlayer();
+                    $name = $player->getName();
+          }
+                    
+          public function onCommand(CommandSender $sender,Command $cmd,$label,array $args){
+                    if($cmd->getName() == "help"){ 
+                    $sender->sendMessage("");
+                    }
+          return true;
+          }
           public function onDisable(){
                     $this->getLogger()->info("Plugin Disabled");
           }
