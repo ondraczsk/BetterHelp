06bb495248dcd4826365a1901b233290ba522e1b
ondraczsk <ondrakraftak@gmail.com>
2016-12-12 13:24:57 +0100

    Update BetterHelp.php

--- src/BetterHelp/BetterHelp.php
+++ src/BetterHelp/BetterHelp.php
@@ -2,42 +2,25 @@
 
 namespace BetterHelp;
 
-use pocketmine\nplugin\PharPluginLoader;
 use pocketmine\plugin\PluginBase;
-use pocketmine\command\Command;
-use pocketmine\command\CommandExecutor;
-use pocketmine\utils\Config;
-use pocketmine\command\CommandSender;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\Player;
 use pocketmine\Server;
+use pocketmine\event\Listener;
 
-// My first non-copied plugin! I learning from Plugin school web :P
+class BetterHelp extends PluginBase implements Listener{
 
-class BetterHelp extends PluginBase {
-  public function onLoad(){
-    $this->getLogger()->info("Plugin Napoveda se nacita..");
-  }
-  public function onEnable(){
-    $this->getLogger()->info("Plugin Napoveda zapnut..");
-    getServer()->getPluginManager()->registerEvents($this,$this);
-    @mkdir($this->getDataFolder()); 
-    $this->config = new Config ($this->getDataFolder() . "config.yml" , Config::YAML, array());
-    
-  }
-  
-
-  public function onJoin(PlayerJoinEvent $event){ $player = $event->getPlayer(); $name = $player->getName();
-                                                 
-  public function onCommand(CommandSender $sender,Command $cmd,$label,array $args){
-    if($cmd->getName() == "help")
-      { $sender->sendMessage("Test!"); } return true;
-  }
-  
-  }
-  
-  
-  public function onDisable(){
-    $this->getLogger()->info("Plugin Napoveda vypnut..");
-  }
-
-   
+          public function onLoad(){
+                    $this->getLogger()->info("Plugin Loading");
+          }
+          public function onEnable(){
+                    $this->getLogger()->info("Enabled Plugin");
+                    $this->getServer()->getPluginManager()->registerEvents($this,$this);
+          }
+          public function onJoin(PlayerJoinEvent $event){
+   $player = $event->getPlayer();
+   $name = $player->getName();
+          public function onDisable(){
+                    $this->getLogger()->info("Plugin Disabled");
+          }
 }
