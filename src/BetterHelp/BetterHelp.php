fdf03aaeb2e6c9ec220d48c8494cf7034257db25
ondraczsk <ondrakraftak@gmail.com>
2017-05-14 13:46:26 +0200

    Update BetterHelp.php

--- src/BetterHelp/BetterHelp.php
+++ src/BetterHelp/BetterHelp.php
@@ -1,27 +1,31 @@
 <?php
-
-namespace BetterHelp;
+///////////////////////////////
+// Copyright 2017, ondraczsk //
+///////////////////////////////
+namespace ondraczsk\BetterHelp;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\Player;
 use pocketmine\utils\TextFormat;
 use pocketmine\Server;
 use pocketmine\event\Listener;
 use pocketmine\utils\Config;
 
-class BetterHelp extends PluginBase implements Listener{
-          
-          
-     /**
-     * Translate Minecraft colors
-     * 
-     * @param char $symbol Color symbol
-     * @param string $message The message to be translated
-     * 
-     * @return string The translated message
-     */
-    public function translateColors($symbol, $message){
+
+class Main extends PluginBase implents Listener {
+
+/*LOADING */
+public function onLoad(){
+$this->getLogger()->info("§f[§eBetter§6Help§f] §eNacitani...");
+}
+
+
+/*COLORS & */
+ public function translateColors($symbol, $message){
     
     	$message = str_replace($symbol."0", TextFormat::BLACK, $message);
     	$message = str_replace($symbol."1", TextFormat::DARK_BLUE, $message);
@@ -46,32 +50,41 @@ class BetterHelp extends PluginBase implements Listener{
     	$message = str_replace($symbol."n", TextFormat::UNDERLINE, $message);
     	$message = str_replace($symbol."o", TextFormat::ITALIC, $message);
     	$message = str_replace($symbol."r", TextFormat::RESET, $message);
-              
-              
+    
     	return $message;
     }
+    
+/*ENABLE*/
+public function onEnable(){
+$this->getServer()->getPluginManager()->registerEvents($this,$this);
+@mkdir($this->getDataFolder()); 
+$this->config = new Config ($this->getDataFolder() . "config.yml" , Config::YAML, array());
+$this->getServer()->getCommandMap()->getCommand("help")->setLabel("helporiginal");
+    $this->getServer()->getCommandMap()->getCommand("help")->unregister($this->getServer()->getCommandMap());
+$this->getLogger()->info("§f[§eBetter§6Help§f] §2Zapnuto!...");
+}
 
-          public function onLoad(){
-                    $this->getLogger()->info("Plugin Loading");
-          }
-          public function onEnable(){
-                    $this->getLogger()->info("Enabled Plugin");
-                    $this->getServer()->getPluginManager()->registerEvents($this,$this);
-                    @mkdir($this->getDataFolder());
-                    $this->config = new Config ($this->getDataFolder() . "config.yml" , Config::YAML, array());
-          }
-          public function onJoin(PlayerJoinEvent $event){
-                    $player = $event->getPlayer();
-                    $name = $player->getName();
-          }
-                    
-          public function onCommand(CommandSender $sender,Command $cmd,$label,array $args){
-                    if($cmd->getName() == "help"){ 
-                    $sender->sendMessage("");
-                    }
-          return true;
-          }
-          public function onDisable(){
-                    $this->getLogger()->info("Plugin Disabled");
-          }
+
+/*DISABLE*/
+public function onDisable(){
+$this->getLogger()->info("§f[§eBetter§6Help§f] §cVypnuto!...");
+}
+       public function onJoin(PlayerJoinEvent $event){ 
+       $player = $event->getPlayer(); 
+       $name = $player->getName(); 
+    }
+
+public function onCommand(CommandSender $sender,Command $cmd,$label,array $args){
+  if($cmd->getName() == "help"){ 
+  if(!isset($args[0])){  
+  $sender->sendMessage("Zadavej: /help ");  
+  }else{ 
+	$cfg = $this->plugin->getConfig()->getAll();	
+	$prefix = "";
+  $player->sendMessage($this->plugin->translateColors("&", $prefix . $cfg["test-message"]));
+      }
+    }
+  }
+  
+  
 }
